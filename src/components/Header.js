2b35a14c7b033bea0bd9587e2dc125dc935f1d77
taylor <taylor.laekeman@gmail.com>
2020-04-22 17:47:54 -0400

    remove boilerplate and add temporary header

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.header`
+  align-items: baseline;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 1.4rem;
+  font-weight: 400;
+  margin: 0;
+`;
+
+const Description = styled.p`
+  font-size: 1rem;
+  font-weight: 400;
+  margin: 0;
+`;
+
+const Header = () => {
+  return (
+    <Container>
+      <Title>Ciao, Estrela Co.</Title>
+      <Description>Admin panel</Description>
+    </Container>
+  );
+};
+
+Header.propTypes = {};
+
+export default Header;
