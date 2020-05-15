2dde99e5ee0f797fe4be7c33951fa6ddddba7eaf
taylor <taylor.laekeman@gmail.com>
2020-05-14 22:17:22 -0400

    update layout on pin creation page

--- src/components/PinCreator.js
+++ src/components/PinCreator.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 
-import Form from 'components/Form';
+import UnstyledForm from 'components/Form';
 import Error from 'components/Form/Error';
 import UnstyledInput from 'components/Form/Input';
 import Submit from 'components/Form/Submit';
 import UnstyledUpload from 'components/Form/Upload';
-import UnstyledLink from 'components/Link';
+import Link from 'components/Link';
 import LoadingIndicator from 'components/LoadingIndicator';
-import Panel from 'components/Panel';
 import { actions as pinActions, selectors as pinSelectors } from 'state/pins';
+
+const Form = styled(UnstyledForm)`
+  ${({ theme }) => theme.components.panel}
+  margin: 0 16px;
+  padding: 32px;
+`;
 
 const getCostError = (cost) => {
   if (!cost || parseInt(cost, 10) <= 0)
@@ -26,16 +31,20 @@ const getNameError = (name) => {
   return name ? null : 'Name is required';
 };
 
-const Input = styled(UnstyledInput)`
-  margin-bottom: 16px;
+const Header = styled.header`
+  padding: 32px;
 `;
 
-const Link = styled(UnstyledLink)`
+const Input = styled(UnstyledInput)`
   margin-bottom: 16px;
 `;
 
 const Upload = styled(UnstyledUpload)`
   margin-bottom: 16px;
+`;
+
+const Wrapper = styled.article`
+  height: 100%;
 `;
 
 const PinCreator = () => {
@@ -58,48 +67,48 @@ const PinCreator = () => {
   };
 
   return (
-    <>
-      <Link to="/listings">Back to listings</Link>
-      <Panel>
-        <Form onSubmit={submitForm}>
-          <Input
-            error={isNameDirty && getNameError(name)}
-            name="Name"
-            onBlur={() => setIsNameDirty(true)}
-            onChange={(newName) => {
-              setName(newName);
-              setIsNameDirty(true);
-            }}
-            value={name}
-          />
-          <Input
-            error={isCostDirty && getCostError(cost)}
-            name="Cost"
-            onBlur={() => setIsCostDirty(true)}
-            onChange={(newCost) => {
-              setCost(newCost);
-              setIsCostDirty(true);
-            }}
-            type="number"
-            value={cost}
-          />
-          <Upload
-            accept=".png"
-            error={isImageDirty && getImageError(image)}
-            onChange={(newImage) => {
-              setImage(newImage);
-              setIsImageDirty(true);
-            }}
-            text="Click to upload an image"
-            value={image}
-          />
-          <Submit disabled={isCreatingPin}>
-            {isCreatingPin ? <LoadingIndicator /> : 'Create pin'}
-          </Submit>
-          {createPinErrors && <Error>Could not create pin</Error>}
-        </Form>
-      </Panel>
-    </>
+    <Wrapper>
+      <Header>
+        <Link to="/listings">Back to listings</Link>
+      </Header>
+      <Form onSubmit={submitForm}>
+        <Input
+          error={isNameDirty && getNameError(name)}
+          name="Name"
+          onBlur={() => setIsNameDirty(true)}
+          onChange={(newName) => {
+            setName(newName);
+            setIsNameDirty(true);
+          }}
+          value={name}
+        />
+        <Input
+          error={isCostDirty && getCostError(cost)}
+          name="Cost"
+          onBlur={() => setIsCostDirty(true)}
+          onChange={(newCost) => {
+            setCost(newCost);
+            setIsCostDirty(true);
+          }}
+          type="number"
+          value={cost}
+        />
+        <Upload
+          accept=".png"
+          error={isImageDirty && getImageError(image)}
+          onChange={(newImage) => {
+            setImage(newImage);
+            setIsImageDirty(true);
+          }}
+          text="Click to upload an image"
+          value={image}
+        />
+        <Submit disabled={isCreatingPin}>
+          {isCreatingPin ? <LoadingIndicator /> : 'Create pin'}
+        </Submit>
+        {createPinErrors && <Error>Could not create pin</Error>}
+      </Form>
+    </Wrapper>
   );
 };
 
