fbf84ed6ef216c5da2e736f35d7c7120dbd25346
taylor <taylor.laekeman@gmail.com>
2020-05-15 21:46:55 -0400

    update header

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 const addAttrs = () => ({ type: 'button' });
 
 const Button = styled.button.attrs(addAttrs)`
-  ${({ theme }) => theme.components.button.primary}
+  ${({ plain, theme }) =>
+    plain ? theme.components.button.plain : theme.components.button.primary}
 `;
 
 export default Button;
